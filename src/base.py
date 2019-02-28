a216b46943f87a064510a46a02f741667453806e
Fryzu <bartek.fryz@gmail.com>
2019-02-28 20:26:31 +0100

    calculating value of 2 photos

--- src/base.py
+++ src/base.py
@@ -42,3 +42,25 @@ class Base(object):
                     f.wtite(" ")
                     f.write(str(picture[1]))
                     f.write("\n")
+
+    def value(self, first, second):
+        '''Calculets points for S and S+1'''
+        
+        setFirst = set(first[2])
+        setSecond = set(second[2])
+
+        #logging.debug("Cacluating value of two: %s %s", setFirst, setSecond)
+
+        intersection = setFirst.intersection(setSecond)
+        onlyInFirst = setFirst - intersection
+        onlyInSecond = setSecond - intersection
+
+        
+        #logging.debug("Intersection %s", intersection)
+        #logging.debug("OnlyInFirst %s", onlyInFirst)
+        #logging.debug("OnlyInSecond %s", onlyInSecond)
+
+        result = min(len(intersection), len(onlyInFirst), len(onlyInSecond))
+        #logging.debug("Points earned %s", result)
+
+        return result
--- src/more_advanced.py
+++ src/more_advanced.py
@@ -5,6 +5,9 @@ import logging
 from itertools import combinations
 
 class Solver(Base):
+    def merge(self, toMerge):
+        return (-1, True, list(set(toMerge[0][2]+toMerge[1][2])))
+
     def solve(self):
         '''creating two groups'''
         self.horizontals = []
@@ -19,5 +22,17 @@ class Solver(Base):
         self.verticals_pairs = list(combinations(self.verticals, 2)) 
         logging.debug("Horizontals: %s", self.horizontals)
         logging.debug("Vertical pairs: %s", self.verticals_pairs)
+        logging.debug("Vertical pairs merged: %s", self.merge(self.verticals_pairs[0]))
+        self.value((-1, True, ["a","b"]), (-1, True, ["b","c"]))
+
+
+
+        ''' Algo starts ''' 
+        self.result.append(self.horizontals[0])
+        del self.horizontals[0]
+        while True:
+            ''' Finding best neighbour '''
+            best_neighbour = None
+            break
 
         return True
